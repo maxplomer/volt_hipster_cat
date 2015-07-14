5b5f1512f347d4126bdb51d0fd401bdd994b1742
Max Plomer <maxplomer@gmail.com>
2015-07-14 01:04:53 -0400

    initial create cats version

--- app/main/models/city.rb
+++ app/main/models/city.rb
@@ -7,19 +7,20 @@ class City < Volt::Model
     ["suburbs", "brooklyn", "LA"].map do |city_name|
       new_city = self.new(name: city_name, game_id: game_id)
       store._cities << new_city
+      Cat.create_cats(store, new_city.id)
       #self.create_cats(store, new_city.id)
       #create_jobs(store, new_city.id)
       #create_apartments(store, new_city.id)
     end
   end
 
-  def self.create_cats(store, city_id)
-    rand(10).times do 
-      new_cat = Cat.create_my_cat
-      new_cat.city_id = city_id
-      store._cats << new_cat
-    end
-  end
+  # def self.create_cats(store, city_id)
+  #   rand(10).times do 
+  #     new_cat = Cat.create_my_cat
+  #     new_cat.city_id = city_id
+  #     store._cats << new_cat
+  #   end
+  # end
 
 
 
