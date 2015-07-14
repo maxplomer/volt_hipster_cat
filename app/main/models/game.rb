d9dd59d8afc6e0461169f5db9286497e68301f3f
Max Plomer <maxplomer@gmail.com>
2015-07-13 23:34:26 -0400

    display current time

--- app/main/models/game.rb
+++ app/main/models/game.rb
@@ -1,6 +1,26 @@
 class Game < Volt::Model
   def play(page)
-    #self._name += 'PLAYED'
     page._playing_a_game = true
+    page._game = self
+    play_round(page)
+    #play_round(page) until game_over?
+    #game_over
+
+    nil
   end
+
+  def play_round(page)
+    #page._current_time = self.current_time
+    page._prompt_user_for_input = true
+  end
+
+
+  def game_over?
+    false
+  end
+
+  # def game_over
+  # end
+
+
 end
