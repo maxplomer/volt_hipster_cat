8b3fbdeffbd189d4ac5adadedb86618df8065bc1
Max Plomer <maxplomer@gmail.com>
2015-10-31 16:48:28 -0400

    commit unsaved changes

--- app/main/models/city.rb
+++ app/main/models/city.rb
@@ -1,14 +1,14 @@
 class City < Volt::Model
   has_many :cats
   #has_many :apartments
-  #has_many :jobs
+  has_many :jobs
 
   def self.create_default_cities(store, game_id)
     ["suburbs", "brooklyn", "LA"].map do |city_name|
       new_city = self.new(name: city_name, game_id: game_id)
       store._cities << new_city
       Cat.create_cats(store, new_city.id)
-      #Job.create_jobs(store, new_city.id)
+      Job.create_jobs(store, new_city.id, city_name)
       #Apartment.create_apartments(store, new_city.id)
     end
   end
